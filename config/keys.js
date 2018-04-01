1afc7bc6317fa1fa5f892cb8113740a624c1a7f7
discoduckling <mitalee.n.desai@gmail.com>
2018-04-01 09:27:42 -0600

    Add google oAuth

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '736343856497-c395grr6p1m984ofpmk44p3l3uo00hro.apps.googleusercontent.com',
+    googleClientSecret: 'XhUVl78tlVfR4AwptBrGyQ5c',
+    mongoURI: 'mongodb://discoduckling:marit5050@ds231229.mlab.com:31229/stock-aggregator-dev',
+    cookieKey: 'alijslifjeij289us'
+}
