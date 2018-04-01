faebb081069d6fd39cb4ea53a40b007dace4b853
discoduckling <mitalee.n.desai@gmail.com>
2018-04-01 13:16:50 -0600

    Add navbar using materialize-css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const header = () => {
     return (
-        <h1>Header</h1>
+        <nav className="#37474f blue-grey darken-3">
+            <div className="nav-wrapper container">
+                <a href="#" className="brand-logo">Logo</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="/dashboard">Try It Out</a></li>
+                    <li><a href="/auth/google">Log In</a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
