c8c6fb18e38e0153f0e16d3f85c6b9bd509cac9c
Mitalee Desai <mitalee.n.desai@gmail.com>
2018-04-13 10:30:23 -0600

    Add auth0 authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,48 @@
 const passport = require('passport');
+const keys = require('../config/keys');
 
 module.exports = app => {
-    app.get('/auth/google',
-        passport.authenticate('google', {
-            scope: ['profile', 'email']
-        })
-    )
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/login', passport.authenticate('auth0', {
+        clientID: keys.CLIENT_ID_AUTH0,
+        domain: keys.AUTH0_DOMAIN,
+        redirectUri: keys.CALLBACK_URL,
+        audience: 'https://' + keys.AUTH0_DOMAIN + '/userinfo',
+        responseType: 'code',
+        scope: 'openid profile'
+    }));
 
-    app.get('/api/current_user', (req, res) => {
-        res.send(req.user);
+    app.get(
+        keys.CALLBACK_URL,
+        passport.authenticate('auth0', {
+            failureRedirect: '/'
+        }), (req, res) => {
+            res.redirect('/');
+        }
+    );
+
+    app.get('/auth/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
     });
 
-    app.get('/api/logout', (req, res) => {
-        req.logout();
+    app.get('/', (req, res) => {
         res.send(req.user);
     })
 }
+// module.exports = app => {
+//     app.get('/auth/google',
+//         passport.authenticate('google', {
+//             scope: ['profile', 'email']
+//         })
+//     )
+//     app.get('/auth/google/callback', passport.authenticate('google'));
+
+//     app.get('/api/current_user', (req, res) => {
+//         res.send(req.user);
+//     });
+
+//     app.get('/api/logout', (req, res) => {
+//         req.logout();
+//         res.send(req.user);
+//     })
+// }
--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,29 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Auth0Strategy = require('passport-auth0');
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
+
+const auth0Strategy = new Auth0Strategy({
+    domain: keys.AUTH0_DOMAIN,
+    clientID: keys.CLIENT_ID_AUTH0,
+    clientSecret: keys.CLIENT_SECRET_AUTH0,
+    callbackURL: keys.CALLBACK_URL
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    User.findOne({ googleId: profile.id })
+        .then(user => {
+            if (user) {
+                done(null, user);
+            } else {
+                new User({ googleId: profile.id }).save()
+                    .then(user => done(null, user));
+            }
+        })
+});
+
+passport.use(auth0Strategy);
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -14,22 +34,31 @@ passport.deserializeUser((id, done) => {
         .then(user => done(null, user))
 });
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback',
-        proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then(existingUser => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user))
-                }
-            })
-    })
-);
+// passport.serializeUser((user, done) => {
+//     done(null, user.id);
+// });
+
+// passport.deserializeUser((id, done) => {
+//     User.findById(id)
+//         .then(user => done(null, user))
+// });
+
+// passport.use(
+//     new GoogleStrategy({
+//         clientID: keys.googleClientID,
+//         clientSecret: keys.googleClientSecret,
+//         callbackURL: '/auth/google/callback',
+//         proxy: true
+//     }, (accessToken, refreshToken, profile, done) => {
+//         User.findOne({ googleId: profile.id })
+//             .then(existingUser => {
+//                 if (existingUser) {
+//                     done(null, existingUser);
+//                 } else {
+//                     new User({ googleId: profile.id })
+//                         .save()
+//                         .then(user => done(null, user))
+//                 }
+//             })
+//     })
+// );
