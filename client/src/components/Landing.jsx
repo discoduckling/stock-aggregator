fe666f03a971b2887e45d6b266f06be540bafa5c
Mitalee Desai <mitalee.n.desai@gmail.com>
2018-04-18 12:11:22 -0600

    Add image to landing page

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
+import TickerImg from '../assets/ticker_screenshot_edit_2.png';
+
 
 const landing = () => {
     return(
-        <div className="container">
-            Landing
-        </div>
+        <Container>
+            <Row>
+                <Col sm='12' md='6' className="landing__box landing__box--left">
+                    An Easier Way to Monitor Your Assets
+                    <div className="landing__btn-group">
+                        <Button size='lg' className='landing__btn'>Get Started!</Button>
+                    </div>
+                </Col>
+                <Col sm='12' md='6' className="landing__box landing__box--right">
+                    <img 
+                        src={TickerImg} 
+                        alt="Example Dashboard" 
+                    />
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
