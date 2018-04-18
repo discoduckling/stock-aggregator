725ce22ac104735475e29d6301a1058b3b59f472
discoduckling <mitalee.n.desai@gmail.com>
2018-04-17 18:42:36 -0600

    Fix keys in prod

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     CLIENT_ID_AUTH0: process.env.CLIENT_ID_AUTH0,
-    CLIENT_SECRET_AUTH0: process.env.CLIENT_ID_AUTH0,
+    CLIENT_SECRET_AUTH0: process.env.CLIENT_SECRET_AUTH0,
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     CALLBACK_URL: process.env.CALLBACK_URL,
     MONGO_URI: process.env.MONGO_URI,
