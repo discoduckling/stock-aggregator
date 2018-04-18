abc2e9117499403b662b41cb48fba05c314a48f8
discoduckling <mitalee.n.desai@gmail.com>
2018-04-17 18:50:48 -0600

    Test prod keys using dev keys

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,17 @@
+// module.exports = {
+//     CLIENT_ID_AUTH0: process.env.CLIENT_ID_AUTH0,
+//     CLIENT_SECRET_AUTH0: process.env.CLIENT_SECRET_AUTH0,
+//     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+//     CALLBACK_URL: process.env.CALLBACK_URL,
+//     MONGO_URI: process.env.MONGO_URI,
+//     COOKIE_KEY: process.env.COOKIE_KEY
+// }
+
 module.exports = {
-    CLIENT_ID_AUTH0: process.env.CLIENT_ID_AUTH0,
-    CLIENT_SECRET_AUTH0: process.env.CLIENT_SECRET_AUTH0,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    CALLBACK_URL: process.env.CALLBACK_URL,
-    MONGO_URI: process.env.MONGO_URI,
-    COOKIE_KEY: process.env.COOKIE_KEY
+    CLIENT_ID_AUTH0: 'gZvRrzryE6TYs7x3oznSkpTYm85UjxPV',
+    CLIENT_SECRET_AUTH0: 'IFM9v0vcKMn6XwSE4tfFgiCMVzC9L97igncStiVkH6MWxt7dogo7ILPEfS4kcIcv',
+    AUTH0_DOMAIN: 'stock-aggregator.auth0.com',
+    CALLBACK_URL: 'https://stock-aggregator-api.herokuapp.com/auth/callback',
+    MONGO_URI: 'mongodb://stock-aggregator-admin:password123@ds231229.mlab.com:31229/stock-aggregator-dev',
+    COOKIE_KEY: 'sjflijelijliejfli'
 }
