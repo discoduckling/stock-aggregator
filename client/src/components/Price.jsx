7a4f58d729b3df64d8d01b6a9133ccfb2422f29d
discoduckling <mitalee.n.desai@gmail.com>
2018-04-18 20:29:57 -0600

    Correct row input add functionality

--- client/src/components/Price.jsx
+++ client/src/components/Price.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-// import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
 
 const price = (props) => {
     return (
-        // <div>
             <div className="price">
-            <IconButton
-                tooltip={`Delete ${props.ticker}`}
-                tooltipPosition="top-left"
-                onClick={props.delete}
-            >
-                <i className="material-icons">clear</i>
-            </IconButton>
-            <div className="price--text">
-                {/* {props.price} */}
-                {props.ticker_id}
-            </div>
+                <IconButton
+                    tooltip={`Delete ${props.ticker}`}
+                    tooltipPosition="top-left"
+                    onClick={props.delete}
+                >
+                    <i className="material-icons">clear</i>
+                </IconButton>
+                <div className="price--text">
+                    {props.price}
+                </div>
             </div>
     )
 }
